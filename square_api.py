34093b3ddefc9cf693602da645f370ec57057111
Logan Brown <logan@exodusintel.com>
2018-09-05 13:48:40 -0500

    formatting

--- square_api.py
+++ square_api.py
@@ -1013,7 +1013,7 @@ def daily_sales (date):
     full_report    += "\n"
     full_report    += "===RECENT COMPARISONS===\n"
     full_report    += "     =San Jac=\n"
-    full_report    += "%s  %-25s %-8s %-15s\n"%("                              ",date.strftime("%a, %b %-d %Y"),"Value","Difference")
+    full_report    += "%s  %-18s %-15s %-15s\n"%("                              ",date.strftime("%a, %b %-d %Y"),"Value","Difference")
     full_report    += "%s    %-25s %-15s %-15s\n"%(" 3 Week Average:",format_money(sales['sjs_total']),format_money(get_recent_average(reportdate,3)[0]),format_money(sales['sjs_total'] - get_recent_average(reportdate,3)[0]))
     full_report    += "%s    %-25s %-15s %-15s\n"%("3 Month Average:",format_money(sales['sjs_total']),format_money(get_recent_average(reportdate,12)[0]),format_money(sales['sjs_total'] - get_recent_average(reportdate,12)[0]))
     full_report    += "%s    %-25s %-15s %-15s\n"%("      6 Month Best:",format_money(sales['sjs_total']),format_money(get_recent_sales_best(reportdate,26)[0]),format_money(sales['sjs_total'] - get_recent_sales_best(reportdate,26)[0]))
@@ -1022,7 +1022,7 @@ def daily_sales (date):
     full_report    += "%s    %-25s %-15s %-15s\n"%(" 12 Month Worst:",format_money(sales['sjs_total']),format_money(get_recent_sales_worst(reportdate,52)[0]),format_money(sales['sjs_total'] - get_recent_sales_worst(reportdate,52)[0]))
     full_report    += "\n"
     full_report    += "     =Jack's=\n"
-    full_report    += "%s  %-25s %-8s %-15s\n"%("                              ",date.strftime("%a, %b %-d %Y"),"Value","Difference")
+    full_report    += "%s  %-18s %-15s %-15s\n"%("                              ",date.strftime("%a, %b %-d %Y"),"Value","Difference")
     full_report    += "%s    %-25s %-15s %-15s\n"%(" 3 Week Average:",format_money(sales['jacks_total']),format_money(get_recent_average(reportdate,3)[1]),format_money(sales['jacks_total'] - get_recent_average(reportdate,3)[1]))
     full_report    += "%s    %-25s %-15s %-15s\n"%("3 Month Average:",format_money(sales['jacks_total']),format_money(get_recent_average(reportdate,12)[1]),format_money(sales['jacks_total'] - get_recent_average(reportdate,12)[1]))
     full_report    += "%s    %-25s %-15s %-15s\n"%("      6 Month Best:",format_money(sales['jacks_total']),format_money(get_recent_sales_best(reportdate,26)[1]),format_money(sales['jacks_total'] - get_recent_sales_best(reportdate,26)[1]))
