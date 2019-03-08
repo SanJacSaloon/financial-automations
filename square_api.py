fd9f5c6d841aa2f5774ac869e2ce50f79a44d8cc
Logan Brown <logan@exodusintel.com>
2019-03-08 14:59:03 -0600

    squareconnect

--- square_api.py
+++ square_api.py
@@ -186,8 +186,10 @@ def update_item_price (amount):
                 continue
         except:
             pass
-        item = CatalogObject(api_instance.retrieve_catalog_object(object_id=i['id']))    
-
+        #item = CatalogObject(api_instance.retrieve_catalog_object(object_id=i['id']))
+        item = api_instance.retrieve_catalog_object(object_id=i['id'])
+        print item.to_dict()
+        #var = CatalogItemVariation(item.item_variation_data())
         var = item.item_variation_data()
         print var
         mon = Money(var.price_money())
