f5aa31b8767c2f6c945c80be5e2fa5f88745348a
Logan Brown <logan@exodusintel.com>
2019-03-08 13:23:28 -0600

    squareconnect

--- square_api.py
+++ square_api.py
@@ -24,15 +24,16 @@ import sys
 
 import squareconnect
 from squareconnect.rest import ApiException
-from squareconnect.apis.location_api import LocationApi
+from squareconnect.apis.locations_api import LocationsApi
 
 # create an instance of the Location API class
-api_instance = LocationApi()
-access_token = secrets["square"]["access_token"]
+api_instance = LocationsApi()
+# setup authorization
+api_instance.api_client.configuration.access_token = secrets["square"]["access_token"]
 
 try:
     # ListLocations
-    api_response = api_instance.list_locations(access_token)
+    api_response = api_instance.list_locations()
     print (api_response.locations)
 except ApiException as e:
     print ('Exception when calling LocationApi->list_locations: %s\n' % e)
